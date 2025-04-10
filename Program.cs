b12bb79c70afbbf7159b0bb3b0e7eb60c77c0f6e
Taylo <Taylo@MARIEL>
2025-04-09 21:29:02 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+
+
+        // ask user to input number
+
+        Console.WriteLine("Enter the first number");
+
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int num3 = num1 + num2;
+        Console.WriteLine("The Result is :" + num3);
+
+    }
+}
